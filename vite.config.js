8c0efdf95c3bd8b095d9510bdf92fdbdad464340
bea_lazo <beatamayo90@gmail.com>
2022-11-16 16:53:13 +0100

    gh-pages added

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://bealazo.github.io/",
+  base: '/tienda-belleza/',
   plugins: [react()]
   
 }
